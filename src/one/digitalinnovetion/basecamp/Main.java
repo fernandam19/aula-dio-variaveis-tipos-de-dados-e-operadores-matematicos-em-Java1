9856569b0c80b5b02ed732a1a8c7f350ef179bbe
Fernandam19 <fernandamelor19@gmail.com>
2022-07-18 21:43:58 -0300

    Initial commit

---
+++ src/one/digitalinnovetion/basecamp/Main.java
@@ -0,0 +1,53 @@
+package one.digitalinnovetion.basecamp;
+
+/**
+ * Classe de exemplo para exercício  da Aula 1 de variáveis,
+ * Tipos de dados e operadores aritiméticos,*/
+public class Main {
+
+    public static void main(String[] args) {
+
+        int  i;
+        int  I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+     final  int  j = 10;
+       // j = 15;
+        int asrn24678nd;
+        //int asrn246 78nd;
+        int asrn2$4678_nd  = 10;
+        //int asrn2$46%78_nd = 10;;
+
+asrn24678nd =100;
+asrn2$4678_nd = 10;
+
+     int quantidadeProduto = 50;
+     // int QuantidadeProduto;
+      final int NUMERO_TENTATIVAS = 5;
+      //final int  numeroTentativas = 5;
+      int QUANTIDADE_OPCOES = 25;
+      //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678nd);
+        System.out.println(asrn2$4678_nd);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+    }
+}
